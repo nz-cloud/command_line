07d50b8414e466e040c8a627540b50caf6e1ac5b
nicolasML <nlima3297@gmail.com>
2024-05-30 18:59:06 -0300

    searching name of my repository

--- pesquisar.py
+++ pesquisar.py
@@ -6,5 +6,6 @@ def pesquisar_my_repositorio():
     for x in range(7):
         pyautogui.press("tab")
     sleep(2)
+    pyautogui.press("enter")
     pyautogui.write("command_line")
     pyautogui.press("enter")
